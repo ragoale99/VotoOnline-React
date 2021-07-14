622d7bd21c70b17b69ba3f5c4cc7ad6c83c76989
Raghesh99 <raghesh99@gmail.com>
2021-07-14 13:00:09 +0200

    up

--- src/components/genericUser/GenericContent.js
+++ src/components/genericUser/GenericContent.js
@@ -1,12 +1,15 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { VotationsContext } from "../../App";
 import { Col, Row, Container, Card } from "react-bootstrap";
 import moment from "moment";
+import Votation from "./Votation";
 import "./GenericContent.css";
 
 export default function GenericContent() {
 	// eslint-disable-next-line no-unused-vars
 	const { votations, changeVotations } = useContext(VotationsContext);
+	const [open, setOpen] = useState(false);
+	const [selectedVotation, setSelectedVotation] = useState(null);
 
 	const votationsToDo = votations.filter((votation) => {
 		return votation.voted === false;
@@ -15,64 +18,82 @@ export default function GenericContent() {
 	const votationsDone = votations.filter((votation) => {
 		return votation.voted !== false;
 	});
+
+	const openVotation = (votation) => {
+		setOpen(true);
+		setSelectedVotation(votation);
+	};
 	return (
-		<Container className="generic-content">
-			<div className="mt-3 from-left">
-				<Row>
-					<Col className="centered py-2">
-						<h2>Votationi disponibili</h2>
-					</Col>
-				</Row>
-				<Row>
-					{votationsToDo.map((votation) => {
-						return (
-							<Col xs={12} md={6} lg={4} key={votation.id}>
-								<Card className="mb-3 card-votations p-1">
-									<Card.Body>
-										<Card.Title>{votation.title}</Card.Title>
-										<Card.Subtitle className="mb-2 text-muted">
-											<em>
-												Dal {moment(votation.dateStart).format("DD/MM/YYYY")} al{" "}
-												{moment(votation.dateEnd).format("DD/MM/YYYY")}
-											</em>
-										</Card.Subtitle>
-										<Card.Text className="mb-4">
-											{votation.description}
-										</Card.Text>
-									</Card.Body>
-								</Card>
+		<>
+			{!open && (
+				<Container className="generic-content mb-2">
+					<div className="mt-3 from-left">
+						<Row>
+							<Col className="centered py-2">
+								<h2 className="title-cards">Votationi disponibili</h2>
 							</Col>
-						);
-					})}
-				</Row>
-			</div>
-			<div className="mt-3 from-right">
-				<Row>
-					<Col className="centered py-2">
-						<h2>Votationi già concluse</h2>
-					</Col>
-				</Row>
-				<Row>
-					{votationsDone.map((votation) => {
-						return (
-							<Col xs={12} md={6} lg={4} key={votation.id}>
-								<Card className="mb-3 card-votations">
-									<Card.Body>
-										<Card.Title>{votation.title}</Card.Title>
-										<Card.Subtitle className="mb-2 text-muted">
-											Dal {moment(votation.dateStart).format("DD/MM/YYYY")} al{" "}
-											{moment(votation.dateEnd).format("DD/MM/YYYY")}
-										</Card.Subtitle>
-										<Card.Text>{votation.description}</Card.Text>
-										<Card.Link href="#">Card Link</Card.Link>
-										<Card.Link href="#">Another Link</Card.Link>
-									</Card.Body>
-								</Card>
+						</Row>
+						<Row>
+							{votationsToDo.map((votation) => {
+								return (
+									<Col
+										xs={12}
+										md={6}
+										lg={4}
+										key={votation.id}
+										onClick={() => openVotation(votation)}>
+										<Card className="mb-3 card-votations p-1">
+											<Card.Body>
+												<Card.Title>{votation.title}</Card.Title>
+												<Card.Subtitle className="mb-2 text-muted">
+													<em>
+														Dal{" "}
+														{moment(votation.dateStart).format("DD/MM/YYYY")} al{" "}
+														{moment(votation.dateEnd).format("DD/MM/YYYY")}
+													</em>
+												</Card.Subtitle>
+												<Card.Text className="mb-4">
+													{votation.description}
+												</Card.Text>
+											</Card.Body>
+										</Card>
+									</Col>
+								);
+							})}
+						</Row>
+					</div>
+					<div className="mt-3 from-right">
+						<Row>
+							<Col className="centered py-2">
+								<h2 className="title-cards">Votationi già concluse</h2>
 							</Col>
-						);
-					})}
-				</Row>
-			</div>
-		</Container>
+						</Row>
+						<Row>
+							{votationsDone.map((votation) => {
+								return (
+									<Col xs={12} md={6} lg={4} key={votation.id}>
+										<Card className="mb-3 card-votations p-1">
+											<Card.Body>
+												<Card.Title>{votation.title}</Card.Title>
+												<Card.Subtitle className="mb-2 text-muted">
+													<em>
+														Dal{" "}
+														{moment(votation.dateStart).format("DD/MM/YYYY")} al{" "}
+														{moment(votation.dateEnd).format("DD/MM/YYYY")}
+													</em>
+												</Card.Subtitle>
+												<Card.Text>{votation.description}</Card.Text>
+											</Card.Body>
+										</Card>
+									</Col>
+								);
+							})}
+						</Row>
+					</div>
+				</Container>
+			)}
+
+			{open && <Votation votation={selectedVotation}></Votation>}
+		</>
 	);
 }
---
+++ src/components/genericUser/Votation.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Votation(props) {
+	return (
+		<div>
+			<p>{props.votation.title}</p>
+		</div>
+	);
+}
