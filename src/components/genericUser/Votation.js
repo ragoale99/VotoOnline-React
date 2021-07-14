696b6a9b01f91a6e9293278713e7db080ec8bfc5
Raghesh99 <raghesh99@gmail.com>
2021-07-14 17:00:06 +0200

    some fix css

--- src/components/genericUser/Votation.js
+++ src/components/genericUser/Votation.js
@@ -1,9 +1,29 @@
-import React from "react";
-import { Col, Row, Container, Card, Button } from "react-bootstrap";
+import React, { useState } from "react";
+import {
+	Col,
+	Row,
+	Container,
+	Card,
+	Button,
+	OverlayTrigger,
+	Tooltip,
+} from "react-bootstrap";
+import { FaVoteYea } from "react-icons/fa";
 import "./Votation.css";
 
 export default function Votation(props) {
 	const { openVotation, votation } = props;
+	const [vote, setVote] = useState(null);
+
+	const checkLength = () => {
+		if (votation.options.length === 2) return 6;
+		else if (votation.options.length > 6) return 3;
+		else return 4;
+	};
+
+	const selectCard = (option) => {
+		setVote(option);
+	};
 	return (
 		<>
 			{openVotation && (
@@ -12,11 +32,13 @@ export default function Votation(props) {
 						<Row className="mt-4">
 							{votation.options.map((option, index) => {
 								return (
-									<Col xs={12} md={6} lg={4} key={option.nome}>
+									<Col xs={12} md={6} lg={checkLength()} key={option.nome}>
 										<Card
-											className={`card-option p-1 ${
+											tabIndex="0"
+											className={`card-option p-1 disable-select ${
 												index % 2 === 0 ? "from-left" : "from-right"
-											}`}>
+											} ${vote === option ? "selected" : "not-selected"}`}
+											onClick={() => selectCard(option)}>
 											<div className="flex-container">
 												<img
 													variant="top"
@@ -38,10 +60,41 @@ export default function Votation(props) {
 						</Row>
 					</div>
 					<div className="flex-container" style={{ flexDirection: "column" }}>
-						<p className="mb-2">Stai vaotando:</p>
-						<Button variant="success" className="">
-							Termina votazione
-						</Button>
+						{!vote ? (
+							<OverlayTrigger
+								placement="top"
+								overlay={
+									<Tooltip id="tooltips">
+										Bottone disabilitato! Clicca su una carta per votare.
+									</Tooltip>
+								}>
+								<span className="d-inline-block" style={{ width: "100%" }}>
+									<div
+										className="flex-container"
+										style={{ flexDirection: "column" }}>
+										<Button
+											variant="primary"
+											className="mb-5 px-3 py-2"
+											disabled
+											aria-disabled="true"
+											style={{ pointerEvents: "none" }}>
+											<FaVoteYea size={20} className="mr-2 mb-1" />
+											Termina votazione
+										</Button>
+									</div>
+								</span>
+							</OverlayTrigger>
+						) : (
+							<>
+								<p className="mb-2">
+									Stai votando: <strong>{vote.nome}</strong>
+								</p>
+								<Button variant="primary" className="mb-5 px-3 py-2">
+									<FaVoteYea size={20} className="mr-2 mb-1" />
+									Termina votazione
+								</Button>
+							</>
+						)}
 					</div>
 				</Container>
 			)}
