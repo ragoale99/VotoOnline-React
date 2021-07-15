07c0e81921cbc2de3409b75fcbf9f70736f02288
Raghesh99 <raghesh99@gmail.com>
2021-07-15 11:06:21 +0200

    rules added

--- src/components/genericUser/Votation.js
+++ src/components/genericUser/Votation.js
@@ -15,6 +15,7 @@ import "./Votation.css";
 export default function Votation(props) {
 	const votations = useContext(VotationsContext);
 	const { setOpenVotation, openVotation, votation } = props;
+	const [showRules, setShowRules] = useState(false);
 	const [vote, setVote] = useState(null);
 
 	const checkLength = () => {
@@ -28,15 +29,65 @@ export default function Votation(props) {
 		else setVote(null);
 	};
 
-	const handleEndVotation = () => {
+	const handlerEndVotation = () => {
 		let elToChange = votations.find((vot) => vot === votation);
 		elToChange.voted = true;
 		elToChange.result = vote.nome;
 		console.log(votations);
 		setOpenVotation(false);
 	};
+
+	const handlerShowRules = () => {
+		setShowRules(!showRules);
+	};
 	return (
 		<>
+			<Container className="">
+				<Row className="mt-4">
+					<Col xs={12} className="centered py-2">
+						<h1>Votazione: {votation.title}</h1>
+					</Col>
+				</Row>
+				<Row>
+					<Col xs={12} className="centered py-2">
+						<p className="rules" onClick={handlerShowRules}>
+							{!showRules ? "Apri" : "Chiudi"} le regole da seguire per la
+							votazione
+							{!showRules ? (
+								<img src="/down.png" alt="" className="dropdown-img ml-1" />
+							) : (
+								<img src="/up.png" alt="" className="dropdown-img ml-1" />
+							)}
+						</p>
+					</Col>
+				</Row>
+				<Row>
+					<Col xs={12} className="centered py-2">
+						{showRules && (
+							<ul>
+								<li>
+									È possibile votare <strong>un solo</strong> partito.
+								</li>
+								<li>
+									Per votare è necessario fare un
+									<strong> click (da PC)</strong> o un
+									<strong> tap (da cellulare)</strong> sulla scelta da votare.
+									Si può fare nuovamente la stessa operazione sulla carta scelta
+									per <strong>annulare</strong> il voto e sceglierne un'altra.
+								</li>
+								<li>
+									Una volta fatta la scelta, cliccare sul bottone
+									<strong> "Termina votazione"</strong>. A questo punto si aprià
+									una finestra in cui si potrà confermare la votazione o tornare
+									indietro. Tale bottone rimarrà <strong> disabilitato</strong>{" "}
+									fino a che non si avrà fatto una scelta.
+								</li>
+							</ul>
+						)}
+					</Col>
+				</Row>
+			</Container>
+
 			{openVotation && (
 				<Container className="votation-content mb-2">
 					<div className="mt-3">
@@ -115,7 +166,7 @@ export default function Votation(props) {
 								<Button
 									variant="primary"
 									className="mb-5 px-3 py-2"
-									onClick={handleEndVotation}>
+									onClick={handlerEndVotation}>
 									<FaVoteYea size={20} className="mr-2 mb-1" />
 									Termina votazione
 								</Button>
