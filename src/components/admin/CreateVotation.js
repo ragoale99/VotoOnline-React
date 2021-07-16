00ba8bbf34bccc3d0a885898acbfd066ddbd9a18
Alessandro Rago <raghesh99@gmail.com>
2021-07-16 17:35:51 +0200

    update

--- src/components/admin/CreateVotation.js
+++ src/components/admin/CreateVotation.js
@@ -1,15 +1,34 @@
 import React, { useState, useContext } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { BiAddToQueue } from "react-icons/bi";
+import { IoIosAdd } from "react-icons/io";
 import { Col, Row, Container, Button, Form, Card } from "react-bootstrap";
 import "./CreateVotation.css";
 import { VotationsContext } from "../../App";
 
 export default function CreateVotation() {
+	// eslint-disable-next-line no-unused-vars
 	const { votations, changeVotations } = useContext(VotationsContext);
 	const [startDate, setStartDate] = useState(new Date());
+	const [options, setOptions] = useState([
+		{
+			nome: "",
+			imagePath: null,
+		},
+		{
+			nome: "",
+			imagePath: null,
+		},
+	]);
 	const [charsCount, setCharsCount] = useState(0);
 	const [endDate, setEndDate] = useState(new Date());
+
+	const addCard = () => {
+		setOptions([...options, { nome: "", imagePath: null }]);
+		console.log(options);
+	};
+
 	const formSubmitHandler = (event) => {
 		event.preventDefault();
 	};
@@ -35,7 +54,7 @@ export default function CreateVotation() {
 					<Form.Control
 						as="textarea"
 						rows={3}
-						maxlength="100"
+						maxLength="100"
 						placeholder="Inserisci la descrizione"
 						required
 						onChange={(e) => setCharsCount(e.target.value.length)}
@@ -66,21 +85,58 @@ export default function CreateVotation() {
 						/>
 					</div>
 				</div>
-				<h3 className="mt-5 centered">Inseirisci scelte</h3>
+				<h3 className="mt-5 centered">Inserisci scelte</h3>
 				<Row>
-					{votations.map((votation, index) => {
+					{options.map((option, index) => {
 						return (
-							<Col xs={12} key={votation.id}>
-								<Card className="mt-3	">
+							<Col xs={12} key={index}>
+								<Card className="mt-3	carta">
 									<Card.Body>
 										<Card.Title className="centered">
 											Carta {index + 1}
 										</Card.Title>
+										<Form.Group>
+											<Form.Label>
+												<strong>Nome</strong>
+											</Form.Label>
+											<Form.Control
+												type="text"
+												placeholder="Inserisci il titolo"
+												required
+												value={option.nome}
+												className="inp"
+											/>
+										</Form.Group>
+										<Form.Group>
+											<Form.Label>
+												<strong>Immagine</strong>
+											</Form.Label>
+											<Form.Control
+												type="file"
+												placeholder="Inserisci il titolo"
+												required
+												value={option.imagePath}
+												className="inp"
+											/>
+										</Form.Group>
 									</Card.Body>
 								</Card>
 							</Col>
 						);
 					})}
+				</Row>
+				<Row className="mt-3">
+					<Button className="mx-auto ombre-btn" onClick={addCard}>
+						<IoIosAdd size={26} />
+					</Button>
+				</Row>
+				<Row className="mt-4">
+					<Button
+						className="mx-auto ombre-btn py-2 px-4"
+						onClick={addCard}
+						variant="success">
+						<BiAddToQueue className="mr-1" /> Crea votazione
+					</Button>
 				</Row>
 			</Form>
 		</Container>
